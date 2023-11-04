00bfc8de7eafdff487c2e138cc1489a849e21d51
Atahan Ekici <atahanekici@hotmail.com>
2023-11-04 17:37:54 +0300

    Question 264's solution

--- Question_264/Solution.java
+++ Question_264/Solution.java
@@ -1,14 +1,36 @@
 package Question_264;
-import static Question_263.Solution.isUgly;
+// SOLVED //
 public class Solution 
 {
-    public int nthUglyNumber(int n) 
+    public static int nthUglyNumber(int n) 
     {
-        int counter = 0;
-        
-        for(int i=0;counter<n;i++)
+        if (n <= 0) 
         {
-            
+            return 0;
         }
+
+        int[] uglyNumbers = new int[n];
+        uglyNumbers[0] = 1;
+        int index2 = 0, index3 = 0, index5 = 0;
+
+        for (int i = 1; i < n; i++) 
+        {
+            int nextUgly = Math.min(uglyNumbers[index2] * 2, Math.min(uglyNumbers[index3] * 3, uglyNumbers[index5] * 5));
+            uglyNumbers[i] = nextUgly;
+
+            if (nextUgly == uglyNumbers[index2] * 2)
+            {
+                index2++;
+            }
+            if (nextUgly == uglyNumbers[index3] * 3)
+            {
+                index3++;
+            }
+            if (nextUgly == uglyNumbers[index5] * 5) 
+            {
+                index5++;
+            }
+        }
+        return uglyNumbers[n - 1];
     }
 }
